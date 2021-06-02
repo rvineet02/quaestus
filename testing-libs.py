31ae49a69c190635af207ae4b46e07ac8f45f782
vineet <rvineet02@gmail.com>
2021-06-02 16:48:42 +0530

    Gets data from yf in a pd dataframe and then takes only close price info and volume

--- testing-libs.py
+++ testing-libs.py
@@ -1,7 +1,6 @@
 # Adding libraries and installed packages to requirements.txt:
 # pip freeze > requirements.txt
 
-# General packages 
 # import numpy 
 # import scipy 
 # import pandas as pd 
@@ -9,15 +8,47 @@
 # import pyfolio 
 
 
+import pandas as pd 
+
 # For price data 
 import yfinance as yf 
 from yahoofinancials import YahooFinancials
 
 
+# General packages 
 
-# Use of yf to get stock price information
-tsla_info = yf.download('TSLA', start='2019-12-31',end='2020-01-01',progress=False)
-print(tsla_info.head())
+
+
+# Adding functions so code is easier to use: 
+
+
+#getData calls the library and gets the data
+# history is a pandas dataframe object
+def getData(stockName):
+    try:
+        st = yf.Ticker(stockName)
+        history = st.history(period="max", interval="1d")
+    except Exception:
+        print("Exception: " + Exception)
+
+
+    return history
+
+
+# useData gets complete dataframe from getData and only keeps close price and volume
+
+def useData(history):
+    try:
+        df = history[["Close", "Volume"]]
+    except Exception:
+        print("Could not get data: " + Exception)
+    print(df.head())
+    
+
+if __name__ == "__main__":
+    df = getData("MSFT")
+    useData(df)
+    
 
 
 
