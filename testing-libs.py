955bf2ede8df364234632af78b98723f8a5e5ae1
vineet <rvineet02@gmail.com>
2021-06-02 18:12:50 +0530

    RSI calc working and compared values with lib imported from TA-Lib

--- testing-libs.py
+++ testing-libs.py
@@ -7,12 +7,14 @@
 # # import statsmodel 
 # import pyfolio 
 
-
+import datetime 
+import talib
 import pandas as pd 
 
 # For price data 
 import yfinance as yf 
 from yahoofinancials import YahooFinancials
+from yfinance.utils import auto_adjust
 
 
 # General packages 
@@ -31,6 +33,7 @@ def getData(stockName):
     except Exception:
         print("Exception: " + Exception)
 
+    # print(history.columns)
 
     return history
 
@@ -42,14 +45,69 @@ def useData(history):
         df = history[["Close", "Volume"]]
     except Exception:
         print("Could not get data: " + Exception)
-    print(df.head())
+
+    return df 
+
+
+def RSI(df):
+    # Difference in price from previous day
+    price = df["Close"]
+    delta = price.diff()
+    delta = delta[1:]
+
+    # Series for Up (positive gains) and Down (negative gains)
+    up, down = delta.clip(lower=0), -1*delta.clip(upper=0)
+
+    # Calculate the Exponentially Weighted Moving Average 
+    emp_up = up.ewm(com=13, adjust=False).mean()
+    ema_down = down.ewm(com=13, adjust=False).mean()
+
+    rs = emp_up/ema_down
     
+    rsi = 100 - (100/(1+rs))
+
+    return rsi
+
+
+
+# Only have this here to test my own written method
+def TechRSI(df):
+    close = df["Close"]
+    rsi = talib.RSI(close, timeperiod=14)    
+    return rsi
+
+
+def compareRSI():
+
+    mine = RSI(useData(getData("MSFT")))
+    mine.sort_index(inplace=True)
+    tlb = TechRSI(useData(getData("MSFT")))
+    # tlb starts one extra day in 1986 so removing it
+    tlb = tlb[1:]
+    tlb.sort_index(inplace=True)
+
+    # Making the two Series into one df to see values side-by-side    
+    df = pd.concat([mine, tlb], axis=1)
+    print(df.tail())
+
+    # Prints true or false value by value keeping the index 
+    # print(mine != tlb.values)
+
 
 if __name__ == "__main__":
-    df = getData("MSFT")
-    useData(df)
-    
+    df = useData(getData("MSFT"))
 
+    # in_time_my_rsi = datetime.datetime.now()
+    # RSI(df)
+    # out_time_my_rsi = datetime.datetime.now()
+    # print("Time taken for my RSI method: " + str(out_time_my_rsi - in_time_my_rsi))
+    # in_time_import_rsi = datetime.datetime.now()
+    # TechRSI(df)
+    # out_time_import_rsi = datetime.datetime.now()
+    # print("Time taken for imported RSI method: " + str(out_time_import_rsi - in_time_import_rsi)) 
+
+
+    compareRSI()
 
 
 
