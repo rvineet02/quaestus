03eb89b8ff8ff9c92a5d7d94cfa49219d52e1d56
vineet <rvineet02@gmail.com>
2021-06-03 11:29:08 +0530

    Updated file strcuture

---
+++ getting_data.py
@@ -0,0 +1,20 @@
+# Updated file to get data
+
+from dataclasses import dataclass
+import pandas as pd 
+import yfinance as yf 
+import datetime
+
+
+def getData(ticker):
+    try:
+        stock = yf.download(ticker)
+    except Exception:
+        print("Could not getData()")
+    print(stock.columns)
+    return stock
+
+
+
+# if __name__ == "__main__":
+#     getData("TSLA")
