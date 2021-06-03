81c4ffca4cfd011f52de455eddad38e8ed1b9514
vineet <rvineet02@gmail.com>
2021-06-03 12:39:11 +0530

    Working MA crossover for 10D and 50D...need to loop it for all crossovers now

--- technical_analysis.py
+++ technical_analysis.py
@@ -18,8 +18,31 @@ def SimpleMovingAverages(df):
         moving_average = talib.MA(closing_prices, timeperiod=period, matype=0)
         df["MA_"+str(period)] = pd.Series(moving_average, index=df.index)
 
+    return df
+
+
+def SimpleMovingAveragesCrossovers(df):
+    
+
+    # Signal has value of 1 when shorter term MA > longer term MA 
+    # Position finds the daily change in value of Signal
+    # So if position is 1 it is a buy call since it moved from 0 to 1 
+    # else if position is -1 it is a sell call since it moved from 1 to 0
+    # short_term = [10,50,100]
+    # long_term = [50,100,200]
+
+    # for i in range(len(short_term)):
+    #     df["Signal_"+str(short_term[i])+str(long_term[i])] = 0.0
+    #     df["Signal_"+str(short_term[i])+str(long_term[i])][short_term[i]] = np.where(df["MA_"+str(short_term[i])][short_term[i]] > df["MA_"+str(long_term[i])][short_term[i]])
+
+    df['Signal'][10:] = np.where(df['MA_10'][10:] > df['MA_50'][10:], 1.0, 0.0)
+    df["Position"] = df["Signal"].diff()
+
+    # print(df.loc[df["Position"] == 1])
     print(df)
 
 
 if __name__ == "__main__":
-    SimpleMovingAverages(df=gd.getData("TSLA"))
+    # SimpleMovingAverages(df=gd.getData("TSLA"))
+    SimpleMovingAveragesCrossovers(df=SimpleMovingAverages(df=gd.getData("TSLA")))
+
